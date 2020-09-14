bffd8fb141abadc395a9c71b9be723e56a5b4053
Julio Altamiranda <julioaltamiranda77@gmail.com>
2020-09-14 17:42:33 -0300

    some corrections

--- js/index.js
+++ js/index.js
@@ -1,25 +1,23 @@
-$(window).on("load", () => {
-    $(".load-container").fadeOut();
-    $(".bar-btn").click(() => {
-        $(".main").toggleClass('active');
-        $(".sidebar").toggleClass('active');
-    })
-    $(".close-btn").click(() => {
-        $(".sidebar").toggleClass('active');
-    })
-    $(".menu-item").click(event => {
-        $(this).children('a').toggleClass('active');
-        $(this).children('ul').slideToggle();
-        $(this).children('a').children('i').toggleClass('active');
-    });
-    $(".user-profile img").click(() => {
-        $(".card").toggleClass('active');
-    });
-    $(window).resize(() => {
-        if ($(this).width() >= 991) {
-            $(".main").removeClass('active');
-            $(".sidebar").removeClass('active');
-        }
-        console.log($(this).width());
-    });
+$(".load-container").fadeOut();
+$(".bar-btn").click(function () {
+    $(".main").toggleClass('active');
+    $(".sidebar").toggleClass('active');
+});
+$(".close-btn").click(function () {
+    $(".sidebar").toggleClass('active');
+});
+$(".menu-item").click(function () {
+
+    $(this).children('a').children('i').toggleClass('active');
+    $(this).children('ul').slideToggle();
+});
+
+$(".user-profile img").click(function () {
+    $(".card").toggleClass('active');
+});
+$(window).resize(function () {
+    if ($(this).width() >= 991) {
+        $(".main").removeClass('active');
+        $(".sidebar").removeClass('active');
+    }
 });
